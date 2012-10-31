287767631dbbd5ae7e7bf6cb09888faed7913e65
Eric Abouaf <eric.abouaf@gmail.com>
2012-10-31 21:41:46 +0100

    LocationInput and LocationBuilder

--- modules/locationinput/locationinput.js
+++ modules/locationinput/locationinput.js
@@ -5,32 +5,9 @@ exports.worker = function (task, config) {
 
     var input = JSON.parse(task.config.input);
 
-    var url = 'http://maps.googleapis.com/maps/api/geocode/json';
+    var search_location = require('../locationbuilder/locationbuilder').search_location;
 
-    var q = {
-        sensor: 'false',
-        address: input.default || input.debug
-    };
-
-    url += '?' + querystring.stringify(q);
-
-
-    request(url, function (error, response, body) {
-
-        var gmapsResults = JSON.parse(body).results[0];
-
-        // TODO
-        //console.log(JSON.stringify(gmapsResults, null, 3));
-
-        var response = {
-            "country": "United States",
-            "postal": "44114",
-            "state": "OH",
-            "city": "Cleveland",
-            "lat": "41.508315",
-            "lon": "-81.681363",
-            "quality": "60"
-        };
+    search_location(input.default || input.debug, function (response) {
 
         task.respondCompleted({
             _OUTPUT: response
@@ -38,6 +15,5 @@ exports.worker = function (task, config) {
 
     });
 
-    
 };
 
